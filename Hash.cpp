8891cdc9c540e8596666366937496ac1fc2e4671
Jayden Huang <411914@beaverton.k12.or.us>
2022-03-09 14:22:20 -0800

    DeleteWorking?

--- Hash.cpp
+++ Hash.cpp
@@ -21,13 +21,15 @@ public:
   int index;
   Node* head = NULL;
   Node* tail = NULL;
+  int collisioncount = 0;
   void setIndex(int newindex) {
     index = newindex;
   }
   Node* getHead() {
     return head;
   }
-  void add(int newvalue) {
+  void addstudent(int newvalue) {
+    collisioncount ++;
     if(head == NULL) {
       Node* newnode = new Node();
       newnode->value = newvalue;
@@ -40,30 +42,38 @@ public:
     newnode->value = newvalue;
     newnode->next = NULL;
     tail->next = newnode;
-    tail = tail ->next;
+    tail = tail->next;
    }
   }
+  
 };
 //Function prototyping
 int hashfunction(int studentidhash);
-void addStudent(int newnumber, studentinfo newarray[100]);
+void addStudent(int newnumber, studentinfo newarray[]);
 void printStudent(vector <studentinfo*> &v);
-void delStudent(vector <studentinfo*> &v);
+void delStudent(int deletenumber, studentinfo newarray[]);
 int main() {
   //Variable setup
   char commandinput[7];
   bool running = true;
-  studentinfo test[100];
+  studentinfo hashtable[100];
   
   for(int i = 0; i < 100; i++) {
-    test[i].setIndex(i);
+    hashtable[i]= new studentinfo();
+    hashtable[i].setIndex(i);
   }
-  addStudent(0, test);
-  addStudent(100, test);
-  cout << hashfunction(100) << endl;
-  Node* testing = test[0].getHead();
+  addStudent(0, hashtable);
+  addStudent(100, hashtable);
+  addStudent(200, hashtable);
+  addStudent(300, hashtable);
+  addStudent(4, hashtable);
+  delStudent(4, hashtable);
+  addStudent(5000, hashtable);
+  delStudent(100, hashtable);
+  delStudent(200, hashtable);
+  Node* testing = hashtable[0].getHead();
   while(testing != NULL) {
-    cout << testing->value;
+    cout << testing->value << endl;
     testing = testing->next;
   }
   /*
@@ -93,13 +103,32 @@ int hashfunction(int studentidhash) {
   int posthash = studentidhash % 100;
   return posthash;
 }
-void addStudent(int newnumber, studentinfo newarray[100]) {
+void addStudent(int newnumber, studentinfo newarray[]) {
   int bucketnumber = hashfunction(newnumber);
-  newarray[bucketnumber].add(newnumber);
+  newarray[bucketnumber].addstudent(newnumber);
 }
 void printStudent(vector <studentinfo*> &v) {
   
 }
-void delStudent(vector <studentinfo*> &v) {
-  
+void delStudent(int deletenumber, studentinfo newarray[]) {
+  int posthash = hashfunction(deletenumber);
+  Node* current = newarray[posthash].getHead();
+  Node* prev = NULL;
+  while(current != NULL) {
+    if(current->value == deletenumber) {
+      Node* temp = current;
+      if(current->next == NULL) {
+	prev->next = NULL;
+	break;
+      }
+      else {	
+        prev->next = temp->next;
+        return;
+      }
+    }
+    else {
+      prev = current;
+      current = current->next;
+    }  
+  }
 }
